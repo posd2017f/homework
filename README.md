0a4f529858a51d6cea4e4e2d5c6cfad321d03c20
e8315402 <e8315402@gmail.com>
2017-11-01 14:54:11 +0800

    hw5

--- README.md
+++ README.md
@@ -1,82 +1,35 @@
 # POSD2017F Homework
 
-## Homework assignment 4
+## Homework assignment 5
 
 Please use [the files that were used in course](https://github.com/yccheng66/posd2017f) and copy test header to your repository from this project.
 
-:zap: Some people will place header files into `include` folder, and it may cause the building of .ut job to fail. If so, just email to us and we will change the configuation for you.
-
-#### Update
-
-  * Thu Oct 26 2017 09:45:46
-     
-     There are so many students ask us how to test exception, we recommand you just [google it](https://www.google.com.tw/search?q=c%2B%2B+exception+test). And if you already saw the example from google, don't just copy/paste, look the description on test carefully to write the appropriate test.
+In this assignment, you are required to implement the more complete parser through each test. It will use the interface written in the course. And then, there are few tests will need some new methods of different term class, please follow the description of test, implement each method.
    
 #### Assignment requirement
 
-  1.  Implement `List` according to the `list.h`, and also all tests in `utList.h`.
+  1. Complete the parser with the description of each test
 
-  2. Write the corresponding makefile to generate executable file which named `hw4`. Note that it is the executable name, not the test file name.
-
-  3. Make sure your CI jobs are both passed before deadline.
+  2. Implement the new method `arity()`, `args()` in `Struct`.
+  
+  3. Write the corresponding makefile to generate executable file which named hw5. Note that it is the executable name, not the test file name.
+  
+  4. Make sure your CI jobs are both passed before deadline.
 
 #### Marks
 
-  You totally have 19 tests in your own, each one is 2 points.
-  And TA's test also have 19 tests, each one is 3 points.
-  The sum of this homework is 95 points.
+  You totally have 20 tests in your own, each one is 2 points.
+  And TA's test also have 20 tests, each one is 3 points.
+  The sum of this homework is 90 points.
 
 #### Deadline
 
-  Friday Oct 27 2017 23:59:59
+  Wed Nov 8 2017 23:59:59
 
 #### Note
 
-  * About project structure. In pervious homework, some people will place test file into folder and write the corresponding makefile. (e.g. `variable.h` in *include* folder ) This will result the executable in .ut job that still contains your test script but not ours because we just replace the test file at root directory. Thus, the report of test result shows all tests are passed, but in the fact it may isn't. So, if you place your files into some folder, please check the test report in .ut job that test script is still yours or not.
-  
-    **Just email us if you have that situation.**
-
-  * Try to read building information in console log and fix the hw problem  by yourself as possible as you can.
+  * Try to read building information in console log and fix the hw problem by yourself as possible as you can.
   
   * If your CI job can not pull your repository, email to us.
 
 #### Change log
-
- * Tue Oct 24 2017 11:01:43
- 
-   We have already corrected the configuration of .ut job, so you can see the error msg when the test is crash. If you still cannot get the test report and also any error msg, reply to us.
-
- * Mon Oct 23 2017 09:54:36
- 
-   * Please check your ut job, if it's passed but there is no test report comes out (besides first time building), it means your progarm actually is crash. We are discussing about this cas and think about how to fix it or is there other solution. 
-   
-   * And about test case X = [496, X, terence_tao]. In this weekend, we replied one student that this decision was a sample judgment that we made and it can be corrected if it's wrong truly. But it sounds pretty irresponsible(sorry for Mr. X), so we re-discuss this case today.\
-      \
-      About this metter, we need to check the definition of matching. In the section 2.2 *Matching* of the book: PROLOG Programming for Artificial Intelligence (third version, Ivan Bratko, 0-201-40375-7), said if two terms match, it must: (1)They are identical, or (2) the variables in both terms can be instantiated to objects in such a way that after the substitution of variables by these objects the terms become identical. Rule (1) is easy to understand, but what is rule (2)? Rule (2) means that if it can get the any value which can make the equation holds, we said it match. For our case, if variable X could be instantiated by any value that make the equation holds, the #match() should return true, otherwise returns false. **But it couldn't be, and also not yet.**\
-      \
-      We can't set true or false on this case utill the variable X can be instantiated. In SWI-Prolog you will get the string "X = [496, X, terence_tao]" as the result instead of false, but it doesn't mean that this matching actually returns true or false(in program). So, for this one, **we just decided to make it not score. You can get the points but you still need to write the test, we don't care about the true or false it returns.**
-
-  * Sun Oct 22 2017 13:45:41
-  
-    The description on test `Numbers` is wrong, there should be two numbers. Please modify your test if there is only one.
-    
-    ```diff
-    + // Given there are two perfect Numbers: 8128, 496
-    + // When create a new list with the perfect Number
-    + // Then #symbol() of the list should return "[8128, 496]"
-    - // Given there are two perfect Numbers: 8128
-    - // When create a new list with the perfect Number
-    - // Then #symbol() of the list should return "[8128]"
-    ```
-  
-  * Sat Oct 21 2017 11:12:56
-
-    The description on test `headAndTailMatching3` is wrong, the H should return *[first]* and the T returns *[second, third]*.
-
-    ```diff
-    + // ?- [[first], second, third] = [H|T].
-    + // H = [first], T = [second, third].
-    - // ?- [[first], second, third] = [H|T].
-    - // H = first, T = [[second], third].
-    ```
-  
---
+++ utParser.h
@@ -0,0 +1,228 @@
+#ifndef UTPARSER_H
+#define UTPARSER_H
+
+#include "parser.h"
+#include "scanner.h"
+#include "term.h"
+#include "list.h"
+#include "variable.h"
+#include "number.h"
+
+class ParserTest : public ::testing::Test {
+protected:
+  void SetUp() {
+    symtable.clear();
+  }
+};
+
+TEST_F(ParserTest, createTerm_Var){
+  Scanner scanner("X");
+  Parser parser(scanner);
+  ASSERT_EQ("X", parser.createTerm()->symbol());
+}
+
+TEST_F(ParserTest, createTerm_Num){
+  Scanner scanner("123");
+  Parser parser(scanner);
+  ASSERT_EQ("123", parser.createTerm()->symbol());
+}
+
+TEST_F(ParserTest, createTerm_Atom)
+{
+  Scanner scanner("tom");
+  Parser parser(scanner);
+  ASSERT_EQ("tom", parser.createTerm()->symbol());
+}
+
+TEST_F(ParserTest, createArgs)
+{
+  Scanner scanner("1, X, tom");
+  Parser parser(scanner);
+  vector<Term*> terms = parser.getArgs();
+  ASSERT_EQ("1", terms[0]->symbol());
+  ASSERT_EQ("X", terms[1]->symbol());
+  ASSERT_EQ("tom", terms[2]->symbol());
+}
+
+TEST_F(ParserTest, createTerms)
+{
+  Scanner scanner("s(1, X, tom)");
+  Parser parser(scanner);
+  ASSERT_EQ("s(1, X, tom)", parser.createTerm()->symbol());
+}
+
+
+// Given there is string: " 12345,  tom" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return two terms, one is "12345", another is "tom".
+TEST_F(ParserTest, listOfTermsTwo) {
+
+}
+
+
+// Given there is string: "point(1, X, z(1,2,3))" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "point(1, X, z(1,2,3))".
+TEST_F(ParserTest, parseStructOfStruct) {
+
+}
+
+
+// Given there is string: " 12345,  67" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return two terms, one is "12345", another is "67".
+TEST_F(ParserTest, listOfTermsTwoNumbers) {
+
+}
+
+
+// Given there is string: "point(1, X, z)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "point(1, X, z)".
+TEST_F(ParserTest, parseStructThreeArgs) {
+
+}
+
+
+// Given there is string: "   [   ]" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a List.
+// And #symbol() of List should return "[]".
+TEST_F(ParserTest, parseListEmpty) {
+
+}
+
+
+// Given there is string: "_date" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Variable.
+// And #symbol() of Variable should return "_date".
+TEST_F(ParserTest, parseVar) {
+
+}
+
+
+TEST_F(ParserTest, listOfTermsEmpty) {
+
+}
+
+
+// Given there is string: "s(s(s(s(1))))" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "s(s(s(s(1))))".
+TEST_F(ParserTest, parseStructOfStructAllTheWay) {
+
+}
+
+
+// Given there is string: "   [  [1], [] ]" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a List.
+// And #symbol() of List should return "[[1], []]".
+TEST_F(ParserTest, parseListOfLists) {
+
+}
+
+
+// Given there is string: "   [  [1], [], s(s(1)) ]   " which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a List.
+// And #symbol() of List should return "[[1], [], s(s(1))]".
+TEST_F(ParserTest, parseListOfListsAndStruct) {
+
+}
+
+// Given there is string: "   [1, 2]" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a List.
+// And #symbol() of List should return "[1, 2]".
+TEST_F(ParserTest, parseList) {
+
+}
+
+// Given there is string: "[1,2)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a string: "unexpected token" as exception.
+TEST_F(ParserTest, illegal1) {
+
+}
+
+// Given there is string: ".(1,[])" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct which contains two terms.
+// And #arity() of the Struct should be 2.
+// And #symbol() of Struct should return ".(1, [])".
+// And the first term should be number: "1", the second term should be another Strcut: "[]".
+TEST_F(ParserTest, ListAsStruct) {
+
+}
+
+
+// Given there is string: ".(2,.(1,[]))" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct which contains two terms.
+// And #arity() of the Struct should be 2.
+// And #symbol() of Struct should return ".(2, .(1, []))"
+// And the first term should be number: "2", the second term should be another Strcut: ".(1, [])".
+TEST_F(ParserTest, ListAsStruct2) {
+
+}
+
+
+// Given there is string: "s(s(s(s(1)))), b(1,2,3)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return two Struct.
+// And #symbol() of the first Strcut should return "s(s(s(s(1))))".
+// And #symbol() of the second Strcut should return "b(1, 2, 3)".
+TEST_F(ParserTest, parseStructOfStructAllTheWay2) {
+
+}
+
+
+// Given there is string: "point()" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "point()".
+TEST_F(ParserTest, parseStructNoArg) {
+
+}
+
+
+// Given there is string: " 12345,  tom,   Date" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return three terms: "12345", "tom" and "Date".
+TEST_F(ParserTest, listOfTermsThree) {
+
+}
+
+
+// Given there is string: "point(11,12)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "point(11, 12)".
+TEST_F(ParserTest, parseStructTwoArgs) {
+
+}
+
+
+// Given there is string: "...(11,12)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "...(11, 12)".
+TEST_F(ParserTest, parseStructDOTSTwoArgs) {
+
+}
+
+
+// Given there is string: "point(11)" which in scanner.
+// When parser parses all terms via scanner.
+// Then it should return a Struct.
+// And #symbol() of Strcut should return "point(11)".
+TEST_F(ParserTest, parseStructOneArg) {
+
+}
+
+#endif
